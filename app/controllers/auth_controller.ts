74aa23600c2d5b90565de7c0db1b4fa977b2e105
marien <mmanila322@gmail.com>
2025-10-20 18:00:05 +0200

    ref auth : creation compte avec message de confirmation gmail

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,3 +1,4 @@
+import User from '#models/user'
 import { createUserValidator } from '#validators/user'
 import type { HttpContext } from '@adonisjs/core/http'
 import mail from '@adonisjs/mail/services/main'
@@ -7,23 +8,35 @@ export default class AuthController {
     return view.render('pages/auth/register')
   }
 
-  async store({ request}: HttpContext) {
-    
-    const data = await request.validateUsing(createUserValidator)
-    console.log(data);
+  async store({ request, session,response}: HttpContext) {
+    const { fullName, email, password ,confirme } = await request.validateUsing(createUserValidator)
 
+    if(password!=confirme){
+      session.flash('errors', 'les deux mot de passe ne correspondent pas')
+      return response.redirect().back()
+    }
+
+    let user = await User.create({ fullName, email, password })
+
+    await this.send_email(user)
+    session.flash('success', 'User registered successfully')
+    return response.redirect().toRoute("home")
   }
 
 
+   async send_email(user: User) {
+    const code = Math.floor(100000 + Math.random() * 900000).toString()
 
-  
-  async send_email() {
+    // await user.related('verificationCode').create({ code })
+    // sauvegarde
+
+
     await mail.send((message) => {
       message
-        .to("manimamarien08@gmail.com")
-        .from('animamarien08@gmail.com')
-        .subject('Verify your email address')
-        .htmlView('emails/verify_email')
+        .to(user.email)
+        .from(process.env.MAIL_FROM_ADDRESS!)
+        .subject('Vérifiez v otre adresse email')
+        .htmlView('emails/verify_email', { fullName: user.fullName, code })
     })
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -2,13 +2,12 @@
 import AuthController from '#controllers/auth_controller'
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+router.on('/').render('pages/home').as("home")
 
 
 router.get("/register",[AuthController,"register"]).as("register")
 router.post("/store",[AuthController,"store"]).as("store")
 
-router.get("/send_email",[AuthController,"send_email"]).as("send_email")
 
 
 

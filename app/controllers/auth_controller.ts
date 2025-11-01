192119939818cf3a2f0d9e346dd04aeaa69d605a
marien <mmanila322@gmail.com>
2025-11-01 08:53:51 +0100

    la version de test fonctionnell

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -22,11 +22,10 @@ export default class AuthController {
 
       let user = await User.create({ fullName, email, password })
 
-      // await this.send_email(user)
-      // session.flash('success', 'User registered successfully')
-      
-      return response.redirect().toRoute('time_line.show_data')
-      // return response.redirect().toRoute('verify.page')
+       await this.send_email(user)
+       session.flash('success', 'User registered successfully')
+    
+       return response.redirect().toRoute('verify.page')
     } catch (error) {
       session.flash('errors', 'error du serveur')
       return response.redirect().back()
@@ -113,12 +112,10 @@ export default class AuthController {
 
       let user = await User.verifyCredentials(email, password)
 
-
-
-      // if (user.$attributes.is_verify == false) {
-      //  session.flash('errors', 'compte non verifié')
-      //   return response.redirect().toRoute('auth.login')
-      // }
+       if (user.$attributes.is_verify == false) {
+        session.flash('errors', 'compte non verifié')
+         return response.redirect().toRoute('auth.login')
+       }
       await auth.use('web').login(user)
       return response.redirect().toRoute('time_line.show_data')
     } catch (error) {
